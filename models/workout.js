05cc927a8350a0a60130c3cb0e027c7fcedaaa00
Justin Phillips <Jukphillips1990@gmail.com>
2021-09-26 17:17:08 -0600

    created workout model

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+        day: {
+            type: Date, 
+            default: Date.now,
+        },
+        exercises: [
+            {
+            type: {
+            type: String,
+            trim: true,
+            required: "Please enter the type of exercise you would like to submit!"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter in the name of the exercise!"
+            },
+            weight: {
+                type: Number,
+                trim: true,
+            },
+            sets: {
+                type: Number,
+                trim: true,
+            },
+            reps: {
+                type: Number,
+                trim: true,
+            },
+            duration: {
+                type: Number,
+                trim: true,
+            }        
+        },
+        ]
+
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout; 
